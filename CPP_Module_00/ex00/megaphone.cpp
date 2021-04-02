c2d475b2f5b2c3b6c5c6528caab8c2a820b9a6d9
hyeonski <hyeonski@student.42seoul.kr>
2021-04-02 18:23:50 +0900

    00_ex01 finished

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
-int	main()
+char *upper(char *str)
 {
-	std::string input;
+	for (char *it = str; *it != 0; it++)
+	{
+		*it = toupper(*it);
+	}
+	return str;
+}
 
-	std::cin >> input;
-	std::cout << input << std::endl;
+int main(int argc, char** argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+		std::cout << upper(argv[i]);
+	std::cout << std::endl;
 	return 0;
 }
