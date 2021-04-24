ca38dd17b9809b52bf810248c3891037f255d051
hyeonski <hyeonski@student.42seoul.kr>
2021-04-24 21:30:37 +0900

    cpp06 ex01 finished

---
+++ CPP_Module_06/ex00/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <exception>
+
+class ImpossibleConvertException : public std::exception
+{
+public:
+	virtual const char* what() const throw();
+};
+
+const char* ImpossibleConvertException::what() const throw()
+{
+	return ("impossible");
+}
+
+void printChar(double value)
+{
+	std::cout << "char: ";
+	try
+	{
+		if (value < CHAR_MIN || value > CHAR_MAX)
+			throw ImpossibleConvertException();
+		char c = static_cast<char>(value);
+		if (32 <= c && c <= 126)
+			std::cout << "'" << c << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;;
+	}
+}
+
+void printInt(double value)
+{
+	std::cout << "int: ";
+	try
+	{
+		if (value < INT_MIN || value > INT_MAX)
+			throw ImpossibleConvertException();
+		int i = static_cast<int>(value);
+		std::cout << i << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
+
+void printFloat(double value)
+{
+	std::cout << "float: ";
+	try
+	{
+		float f = static_cast<float>(value);
+		if (!isinf(f) && !isnan(f) && (value < -__FLT_MAX__ || value > __FLT_MAX__))
+			throw ImpossibleConvertException();
+		int i = static_cast<int>(value);
+		std::cout << value;
+		if (i - value == 0)
+			std::cout << ".0f" << std::endl;
+		else
+			std::cout << "f" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void printDouble(double value)
+{
+	std::cout << "double: ";
+	std::cout << static_cast<double>(value);
+	
+	int i = static_cast<int>(value);
+	if (i - value == 0)
+		std::cout << ".0" ;
+	std::cout << std::endl;
+}
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		std::cout << "Error: bad arguments\n" << std::endl;
+		return (1);
+	}
+	std::string str = argv[1];
+	double value = atof(str.c_str());
+
+	printChar(value);
+	printInt(value);
+	printFloat(value);
+	printDouble(value);
+}
---
+++ CPP_Module_06/ex01/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+typedef struct Data
+{
+	std::string s1;
+	int n;
+	std::string s2;
+} Data;
+
+std::string* random_string(void)
+{
+	std::string alphanum("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+	char str[8];
+
+	for (int i = 0; i < 8; i++)
+		str[i] = alphanum[rand() % alphanum.length()];
+	return (new std::string(str, 8));
+}
+
+void* serialize(void)
+{
+	char *raw = new char[52];
+
+	std::string *s1 = random_string();
+	int n = rand();
+	std::string *s2 = random_string();
+	*(reinterpret_cast<std::string *>(raw)) = *s1;
+	*(reinterpret_cast<int *>(raw + 24)) = n;
+	*(reinterpret_cast<std::string *>(raw + 32)) = *s2;
+	std::cout << "serialized s1: " << *s1 << " | n : " << n << " | s2 : " << *s2 << std::endl;
+	delete s1;
+	delete s2;
+	return (raw);
+}
+
+Data *deserialize(void *raw)
+{
+	Data *result = new Data;
+
+	result->s1 = std::string(*(reinterpret_cast<std::string *>(raw)));
+	result->n = *(reinterpret_cast<int *>(static_cast<char *>(raw) + 24));
+	result->s2 = std::string(*(reinterpret_cast<std::string *>(static_cast<char *>(raw) + 32)));
+	return (result);
+}
+
+int main(void)
+{
+	srand(time(NULL));
+
+	void *test = serialize();
+	Data *result = deserialize(test);
+
+	std::cout << "Deserializing s1: " << result->s1 << " / n : " << result->n << " / s2 : " << result->s2 << std::endl;
+	
+	delete result;
+	delete static_cast<char *>(test);
+	return 0;
+}
